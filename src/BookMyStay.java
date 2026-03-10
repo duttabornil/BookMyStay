9121c44e2f3d354b1acc54791c026c1b4531655e
duttabornil <duttabornil@gmail.com>
2026-03-10 14:07:47 +0530

    Base application setup

---
+++ src/BookMyStay.java
@@ -0,0 +1,6 @@
+//import java.util.Scanner;
+public class BookMyStay {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System\nSystem initialized successfully.");
+    }
+}
